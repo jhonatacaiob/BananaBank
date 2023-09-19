02ca9ab6076dc628e8d2c413dc77d8ee57550c98
Jhonata Caio <jhonatacaiob@gmail.com>
2023-09-19 15:49:22 -0300

    Inserindo validação no changeset

--- lib/banana_bank_web/controllers/welcome_controller.ex
+++ lib/banana_bank_web/controllers/welcome_controller.ex
@@ -1,7 +1,7 @@
 defmodule BananaBankWeb.WelcomeController do
   use BananaBankWeb, :controller
 
-  def index(conn, params) do    
+  def index(conn, params) do
     conn
     |> put_status(:ok)
     |> json(%{message: "NBem vino ao BananaBank", status: :ok})
