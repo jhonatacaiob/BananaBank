798b77938f0a004e15faecc87fc342c003bbcd9b
Jhonata Caio <jhonatacaiob@gmail.com>
2023-09-19 18:58:29 -0300

    Adicionando criptografia de senha com Pbkdf2

--- lib/banana_bank/users/users.ex
+++ lib/banana_bank/users/users.ex
@@ -2,10 +2,11 @@ defmodule BananaBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @required_params [:name, :password_hash, :email, :cep]
+  @required_params [:name, :password, :email, :cep]
 
   schema "users" do
     field(:name, :string)
+    field(:password, :string, virtual: true)
     field(:password_hash, :string)
     field(:email, :string)
     field(:cep, :string)
@@ -20,5 +21,20 @@ defmodule BananaBank.Users.User do
     |> validate_length(:name, min: 3)
     |> validate_format(:email, ~r/@/)
     |> validate_length(:cep, is: 8)
+    |> validate_length(:password, min: 6)
+    |> add_password_hash()
   end
+
+  defp add_password_hash(
+    %Ecto.Changeset{
+      valid?: true,
+      changes: %{password: password}
+    } = changeset
+  ) do
+    password_hash = Pbkdf2.hash_pwd_salt(password)
+
+    change(changeset, %{password_hash: password_hash})
+  end
+
+  defp add_password_hash(changeset), do: changeset
 end
